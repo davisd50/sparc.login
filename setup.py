61f250141b730a00d903aa5a9edaeaf28a31e852
David Davis <davisd50@gmail.com>
2015-10-26 16:27:37 -0400

    update file structure for pypi

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+setup(name='sparc.login',
+      version=version,
+      description="Authentication and login event packages for Python processing",
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+      ],
+      keywords=['zca'],
+      author='David Davis',
+      author_email='davisd50@gmail.com',
+      url='https://github.com/davisd50/sparc.db',
+      download_url = '',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['sparc'],
+      include_package_data=True,
+      package_data = {
+          '': ['*.zcml']
+        },
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'sparc.cache'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
